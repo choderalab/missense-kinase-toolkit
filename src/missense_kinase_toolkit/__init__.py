be18be6cb4d408cd0799bdc8658816982d45edb1
jessicaw9910 <jessbwhite@gmail.com>
2024-02-20 18:57:21 -0500

    removed __init__.py from src/missense_kinase_toolkit

--- src/missense_kinase_toolkit/__init__.py
+++
@@ -1,7 +0,0 @@
-"""An ETL pipeline package to facilitate structure-based ML for human kinase property prediction"""
-
-# Add imports here
-from .missense_kinase_toolkit import *
-
-
-from ._version import __version__
