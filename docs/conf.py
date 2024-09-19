78cfe55cc05c0057b4138d51cbd8239a5ac2fb4e
Jess White <50890758+jessicaw9910@users.noreply.github.com>
2024-09-19 17:28:59 -0400

    Misc (#57)
    * separating out CI badges given monorepo structure
    
    * changed repo extension configuration
    
    * changed max depth given monorepo
    
    * added [tool.setuptools.packages.find] / where = [.] to databases pyproject.toml
    
    * pip install sub-package in readthedocs.yml; may also need to change docs/requirements.txt
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated sphinx-rtd-theme in requirements.txt
    
    * moved requirements.txt forward in readthedocs.yml
    
    * added docs packages to requirements.yaml since requirements.txt seeming to be ignored
    
    * fixed getting started code block
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * excluding tests
    
    * fixed inline code
    
    * max depth 3
    
    * updated to exclude tests directories and contents
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-exclude_patterns = ["*/tests/*", "*/test_*", "*/tests.py", "*/test_*.py"]
+exclude_patterns = ["*/tests", "*/tests/*", "*/test_*", "*/tests.py", "*/test_*.py"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
