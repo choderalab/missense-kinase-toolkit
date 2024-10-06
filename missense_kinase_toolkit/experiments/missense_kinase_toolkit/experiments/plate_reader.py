34004bfaf405b1063b58dee056a9dfe80a3ec1e1
Mark Polk <mark.polk@choderalab.org>
2024-10-05 22:04:40 -0400

    instantiate Experiment Class

---
+++ missense_kinase_toolkit/experiments/missense_kinase_toolkit/experiments/plate_reader.py
@@ -0,0 +1,15 @@
+class Experiment:
+    """Class to process Tecan i-control .XML output."""
+
+    def __init__(
+        self,
+    ) -> None:
+        """Initialize Experiment Class object.
+
+        Parameters
+        ----------
+
+        Attributes
+        ----------
+
+        """
