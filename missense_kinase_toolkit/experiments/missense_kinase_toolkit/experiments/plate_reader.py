b8dce349f529813b0614555af33d11472484f85a
Mark Polk <mark.polk@choderalab.org>
2024-10-05 23:30:45 -0400

    added labels attribute to Experiment Class

--- missense_kinase_toolkit/experiments/missense_kinase_toolkit/experiments/plate_reader.py
+++ missense_kinase_toolkit/experiments/missense_kinase_toolkit/experiments/plate_reader.py
@@ -1,15 +1,34 @@
+import xml.etree.ElementTree as ET
+
+
 class Experiment:
     """Class to process Tecan i-control .XML output."""
 
     def __init__(
         self,
+        filepath
     ) -> None:
         """Initialize Experiment Class object.
 
         Parameters
         ----------
+        filepath : str
+            Filepath to Tecan i-control .XML output.
 
         Attributes
         ----------
+        filepath : str
+            Filepath to Tecan i-control .XML output.
 
         """
+        self.filepath = filepath
+        self.labels = []
+
+        if filepath.lower()[-4:] != '.xml':
+            raise TypeError('Filepath does not point to .xml file')
+        
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+        
+        for section in root.iter('Section'):
+            self.labels.append(section.attrib['Name'])
