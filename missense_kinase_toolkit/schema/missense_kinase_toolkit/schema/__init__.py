8587bd0be0152c7e0598e6fcf678f26f41834967
Jess White <50890758+jessicaw9910@users.noreply.github.com>
2024-12-04 08:43:07 +0100

    added intial schema sub-package (#67)
    * added intial schema sub-package
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ missense_kinase_toolkit/schema/missense_kinase_toolkit/schema/__init__.py
@@ -0,0 +1,5 @@
+"""Module in missense-kinase-toolkit for schema management."""
+
+from importlib.metadata import version
+
+__version__ = version("schema")
---
+++ missense_kinase_toolkit/schema/missense_kinase_toolkit/schema/kinase_schema.py
@@ -0,0 +1,147 @@
+import logging
+from enum import Enum, StrEnum
+
+from pydantic import BaseModel, constr
+
+logger = logging.getLogger(__name__)
+
+
+LIST_PFAM_KD = [
+    "Protein kinase domain",
+    "Protein tyrosine and serine/threonine kinase",
+]
+
+
+class Group(str, Enum):
+    """Enum class for kinase groups."""
+
+    AGC = "AGC"  # Protein Kinase A, G, and C families
+    Atypical = "Atypical"  # Atypical protein kinases
+    CAMK = "CAMK"  # Calcium/calmodulin-dependent protein kinase family
+    CK1 = "CK1"  # Casein kinase 1 family
+    CMGC = "CMGC"  # Cyclin-dependent kinase, Mitogen-activated protein kinase, Glycogen synthase kinase, and CDK-like kinase families
+    RGC = "RGC"  # Receptor guanylate cyclase family
+    STE = "STE"  # Homologs of yeast Sterile 7, Sterile 11, Sterile 20 kinases
+    TK = "TK"  # Tyrosine kinase family
+    TKL = "TKL"  # Tyrosine kinase-like family
+    Other = "Other"  # Other protein kinases
+
+
+class Family(Enum):
+    """Enum class for kinase families (>=5 in KinHub)."""
+
+    STE20 = "STE20"
+    CAMKL = "CAMKL"
+    CDK = "CDK"
+    Eph = "Eph"
+    MAPK = "MAPK"
+    STKR = "STKR"
+    NEK = "NEK"
+    Src = "Src"
+    DYRK = "DYRK"
+    PKC = "PKC"
+    STE11 = "STE11"
+    RSK = "RSK"
+    MLK = "MLK"
+    GRK = "GRK"
+    CK1 = "CK1"
+    DMPK = "DMPK"
+    STE7 = "STE7"
+    PIKK = "PIKK"
+    RSKb = "RSKb"
+    Alpha = "Alpha"
+    Tec = "Tec"
+    CAMK1 = "CAMK1"
+    PDGFR = "PDGFR"
+    ULK = "ULK"
+    DAPK = "DAPK"
+    RAF = "RAF"
+    RIPK = "RIPK"
+    MLCK = "MLCK"
+    PKA = "PKA"
+    MAPKAPK = "MAPKAPK"
+    RGC = "RGC"
+    CDKL = "CDKL"
+    MAST = "MAST"
+    TSSK = "TSSK"
+    ABC1 = "ABC1"
+    PDHK = "PDHK"
+    Jak = "Jak"
+    Jakb = "Jakb"
+    Other = "Other"
+    Null = None
+
+
+KinaseDomainName = StrEnum(
+    "KinaseDomainName", {"KD" + str(idx + 1): kd for idx, kd in enumerate(LIST_PFAM_KD)}
+)
+SeqUniProt = constr(pattern=r"^[ACDEFGHIKLMNPQRSTVWXY]+$")
+"""Pydantic model for UniProt sequence constraints."""
+SeqKLIFS = constr(pattern=r"^[ACDEFGHIKLMNPQRSTVWY\-]{85}$")
+"""Pydantic model for KLIFS pocket sequence constraints."""
+UniProtID = constr(pattern=r"^[A-Z][0-9][A-Z0-9]{3}[0-9]$")
+"""Pydantic model for UniProt ID constraints."""
+
+
+class KinHub(BaseModel):
+    """Pydantic model for KinHub information."""
+
+    kinase_name: str
+    manning_name: list[str]
+    xname: list[str]
+    group: list[Group]
+    family: list[Family]
+
+
+class UniProt(BaseModel):
+    """Pydantic model for UniProt information."""
+
+    canonical_seq: SeqUniProt
+
+
+class KLIFS(BaseModel):
+    """Pydantic model for KLIFS information."""
+
+    gene_name: str
+    name: str
+    full_name: str
+    group: Group
+    family: Family
+    iuphar: int
+    kinase_id: int
+    pocket_seq: SeqKLIFS | None
+
+
+class Pfam(BaseModel):
+    """Pydantic model for Pfam information."""
+
+    domain_name: KinaseDomainName
+    start: int
+    end: int
+    protein_length: int
+    pfam_accession: str
+    in_alphafold: bool
+
+
+class KinCore(BaseModel):
+    """Pydantic model for KinCore information."""
+
+    seq: SeqUniProt
+    start: int | None
+    end: int | None
+    mismatch: list[int] | None
+
+
+class KinaseInfo(BaseModel):
+    """Pydantic model for kinase information."""
+
+    hgnc_name: str
+    uniprot_id: UniProtID
+    KinHub: KinHub
+    UniProt: UniProt
+    KLIFS: KLIFS | None
+    Pfam: Pfam | None
+    KinCore: KinCore | None
+    bool_offset: bool = True
+    KLIFS2UniProtIdx: dict[str, int | None] | None = None
+    KLIFS2UniProtSeq: dict[str, str | None] | None = None
