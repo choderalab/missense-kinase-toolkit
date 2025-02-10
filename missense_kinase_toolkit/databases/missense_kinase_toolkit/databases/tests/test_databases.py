6e3f5f017f137da8561ab055238d68b46bb1bd21
jessicaw9910 <jessbwhite@gmail.com>
2025-02-10 18:28:15 -0500

    added kincore and KLIFSPocket to testing

--- missense_kinase_toolkit/databases/missense_kinase_toolkit/databases/tests/test_databases.py
+++ missense_kinase_toolkit/databases/missense_kinase_toolkit/databases/tests/test_databases.py
@@ -172,8 +172,13 @@ class TestDatabases:
         )
         assert test.hgnc == "ABL1"
 
-    def test_klifs(self):
+    def test_klifs_kincore(self):
         from missense_kinase_toolkit.databases import klifs
+        from missense_kinase_toolkit.databases.uniprot import UniProt
+        from missense_kinase_toolkit.databases.kincore import (
+            extract_pk_fasta_info_as_dict,
+            align_kincore2uniprot,
+        )
 
         dict_egfr = klifs.KinaseInfo("EGFR")._kinase_info
 
@@ -190,6 +195,148 @@ class TestDatabases:
         )
         assert dict_egfr["species"] == "Human"
         assert dict_egfr["uniprot"] == "P00533"
+
+        dict_kincore = extract_pk_fasta_info_as_dict()
+
+        egfr_uniprot = UniProt(dict_egfr["uniprot"])
+
+        egfr_align = align_kincore2uniprot(
+            str_kincore = dict_kincore[dict_egfr["uniprot"]]["seq"],
+            str_uniprot = egfr_uniprot._sequence
+        )
+
+        assert egfr_align["seq"] == 'FKKIKVLGSGAFGTVYKGLWIPEGEKVKIPVAIKELREATSPKANKEILDEAYVMASVDNPHVCRLLGICLTSTVQLITQLMPFGCLLDYVREHKDNIGSQYLLNWCVQIAKGMNYLEDRRLVHRDLAARNVLVKTPQHVKITDFGLAKLLGAEEKEYHAEGGKVPIKWMALESILHRIYTHQSDVWSYGVTVWELMTFGSKPYDGIPASEISSILEKGERLPQPPICTIDVYMIMVKCWMIDADSRPKFRELIIEFSK'
+        assert egfr_align["start"] == 712
+        assert egfr_align["end"] == 970
+        assert egfr_align["mismatch"] is None
+
+        egfr_pocket = klifs.KLIFSPocket(
+            uniprotSeq = egfr_uniprot._sequence,
+            klifsSeq = dict_egfr["pocket"],
+            idx_kd = (egfr_align["start"] - 1, egfr_align["end"] - 1),
+        )
+
+        assert egfr_pocket.list_klifs_substr_actual == ['KVL', 'GSGAFG', 'TVYK', 'VAIKEL', 'EILDEAYVMAS', 'VDPHVCR', 'LLGI', 'QLI', 'T', 'QLM', 'PFGC', 'LLDYVRE', 'YLEDR', 'RLV', 'HRDLAARN', 'VLV', 'I', 'TDFG', 'LA']
+        assert egfr_pocket.list_klifs_substr_match == ['KVL', 'GSGAFG', 'TVYK', 'VAIKEL', 'EILDEAYVMAS', 'VDPHVCR', 'LLGI', 'QLI', 'TQLM', 'QLM', 'PFGC', 'LLDYVRE', 'YLEDR', 'RLV', 'HRDLAARN', 'VLV', 'ITDFG', 'TDFG', 'TDFGLA']
+        assert egfr_pocket.KLIFS2UniProtIdx == {
+            'I:1': 716, 
+            'I:2': 717, 
+            'I:3': 718, 
+            'g.l:4': 719, 
+            'g.l:5': 720, 
+            'g.l:6': 721, 
+            'g.l:7': 722, 
+            'g.l:8': 723, 
+            'g.l:9': 724, 
+            'II:10': 725, 
+            'II:11': 726, 
+            'II:12': 727, 
+            'II:13': 728, 
+            'III:14': 742, 
+            'III:15': 743, 
+            'III:16': 744, 
+            'III:17': 745, 
+            'III:18': 746, 
+            'III:19': 747, 
+            'αC:20': 758, 
+            'αC:21': 759, 
+            'αC:22': 760, 
+            'αC:23': 761, 
+            'αC:24': 762, 
+            'αC:25': 763, 
+            'αC:26': 764, 
+            'αC:27': 765, 
+            'αC:28': 766, 
+            'αC:29': 767, 
+            'αC:30': 768, 
+            'b.l:31': 769, 
+            'b.l:32': 770, 
+            'b.l:33': 772, 
+            'b.l:34': 773, 
+            'b.l:35': 774, 
+            'b.l:36': 775, 
+            'b.l:37': 776, 
+            'IV:38': 777, 
+            'IV:39': 778, 
+            'IV:40': 779, 
+            'IV:41': 780, 
+            'V:42': 787, 
+            'V:43': 788, 
+            'V:44': 789, 
+            'GK:45': 790, 
+            'hinge:46': 791, 
+            'hinge:47': 792, 
+            'hinge:48': 793, 
+            'linker:49': 794, 
+            'linker:50': 795, 
+            'linker:51': 796, 
+            'linker:52': 797, 
+            'αD:53': 798, 
+            'αD:54': 799, 
+            'αD:55': 800, 
+            'αD:56': 801, 
+            'αD:57': 802, 
+            'αD:58': 803, 
+            'αD:59': 804, 
+            'αE:60': 827, 
+            'αE:61': 828, 
+            'αE:62': 829, 
+            'αE:63': 830, 
+            'αE:64': 831, 
+            'VI:65': 832, 
+            'VI:66': 833, 
+            'VI:67': 834, 
+            'c.l:68': 835, 
+            'c.l:69': 836, 
+            'c.l:70': 837, 
+            'c.l:71': 838, 
+            'c.l:72': 839, 
+            'c.l:73': 840, 
+            'c.l:74': 841, 
+            'c.l:75': 842, 
+            'VII:76': 843, 
+            'VII:77': 844, 
+            'VII:78': 845, 
+            'VIII:79': 853, 
+            'xDFG:80': 854, 
+            'xDFG:81': 855, 
+            'xDFG:82': 856, 
+            'xDFG:83': 857, 
+            'a.l:84': 858, 
+            'a.l:85': 859,
+        }
+        assert egfr_pocket.KLIFS2UniProtSq == {
+            'I': 'KVL', 
+            'g.l': 'GSGAFG', 
+            'II': 'TVYK', 
+            'II:III': 'GLWIPEGEKVKIP', 
+            'III': 'VAIKEL', 
+            'III:αC': 'REATSPKANK', 
+            'αC': 'EILDEAYVMAS', 
+            'b.l_1': 'VD', 
+            'b.l_intra': 'N', 
+            'b.l_2': 'PHVCR', 
+            'IV': 'LLGI', 
+            'IV:V': 'CLTSTV', 
+            'V': 'QLI', 
+            'GK': 'T', 
+            'hinge': 'QLM', 
+            'hinge:linker': None, 
+            'linker_1': 'P', 
+            'linker_intra': None, 
+            'linker_2': 'FGC', 
+            'αD': 'LLDYVRE', 
+            'αD:αE': 'HKDNIGSQYLLNWCVQIAKGMN', 
+            'αE': 'YLEDR', 
+            'αE:VI': None, 
+            'VI': 'RLV', 
+            'c.l': 'HRDLAARN', 
+            'VII': 'VLV', 
+            'VII:VIII': 'KTPQHVK', 
+            'VIII': 'I', 
+            'xDFG': 'TDFG', 
+            'a.l': 'LA',
+        }
 
     def test_scrapers(self):
         from missense_kinase_toolkit.databases import scrapers
@@ -312,23 +459,21 @@ class TestDatabases:
             == "Protein tyrosine and serine/threonine kinase"
         )
 
+    def test_protvar():
+        from missense_kinase_toolkit.databases.protvar import ProtvarScore
 
-def test_protvar():
-    from missense_kinase_toolkit.databases.protvar import ProtvarScore
+        temp_obj = ProtvarScore(database="AM", uniprot_id="P00519", pos=292, mut="D")
+        assert len(temp_obj._protvar_score) == 1
+        assert temp_obj._protvar_score[0]["amPathogenicity"] == 0.4217
+        assert temp_obj._protvar_score[0]["amClass"] == "AMBIGUOUS"
 
-    temp_obj = ProtvarScore(database="AM", uniprot_id="P00519", pos=292, mut="D")
-    assert len(temp_obj._protvar_score) == 1
-    assert temp_obj._protvar_score[0]["amPathogenicity"] == 0.4217
-    assert temp_obj._protvar_score[0]["amClass"] == "AMBIGUOUS"
+    def test_ncbi():
+        from missense_kinase_toolkit.databases import ncbi
 
-
-def test_ncbi():
-    from missense_kinase_toolkit.databases import ncbi
-
-    seq_obj = ncbi.ProteinNCBI(accession="EAX02438.1")
-    assert seq_obj.list_headers == [
-        "EAX02438.1 BR serine/threonine kinase 2, isoform CRA_c [Homo sapiens]"
-    ]
-    assert seq_obj.list_seq == [
-        "MTSTGKDGGAQHAQYVGPYRLEKTLGKGQTGLVKLGVHCVTCQKVAIKIVNREKLSESVLMKVEREIAILKLIEHPHVLKLHDVYENKKYLYLVLEHVSGGELFDYLVKKGRLTPKEARKFFRQIISALDFCHSHSICHRDLKPENLLLDEKNNIRIADFGMASLQVGDSLLETSCGSPHYACPEVIRGEKYDGRKADVWSCGVILFALLVGALPFDDDNLRQLLEKVKRGVFHMPHFIPPDCQSLLRGMIEVDAARRLTLEHIQKHIWYIGGKNEPEPEQPIPRKVQIRSLPSLEDIDPDVLDSMHSLGCFRDRNKLLQDLLSEEENQEKMIYFLLLDRKERYPSQEDEDLPPRNEIDPPRKRVDSPMLNRHGKRRPERKSMEVLSVTDGGSPVPARRAIEMAQHGQRSRSISGASSGLSTSPLSSPRVTPHPSPRGSPLPTPKGTPVHTPKESPAGTPNPTPPSSPSVGGVPWRARLNSIKNSFLGSPRFHRRKLQVPTPEEMSNLTPESSPELAKKSWFGNFISLEKEEQIFVVIKDKPLSSIKADIVHAFLSIPSLSHSVISQTSFRAEYKATGGPAVFQKPVKFQVDITYTEGGEAQKENGIYSVTFTLLSGPSRRFKRVVETIQAQLLSTHDPPAAQHLSEPPPPAPGLSWGAGLKGQKVATSYESSL"
-    ]
+        seq_obj = ncbi.ProteinNCBI(accession="EAX02438.1")
+        assert seq_obj.list_headers == [
+            "EAX02438.1 BR serine/threonine kinase 2, isoform CRA_c [Homo sapiens]"
+        ]
+        assert seq_obj.list_seq == [
+            "MTSTGKDGGAQHAQYVGPYRLEKTLGKGQTGLVKLGVHCVTCQKVAIKIVNREKLSESVLMKVEREIAILKLIEHPHVLKLHDVYENKKYLYLVLEHVSGGELFDYLVKKGRLTPKEARKFFRQIISALDFCHSHSICHRDLKPENLLLDEKNNIRIADFGMASLQVGDSLLETSCGSPHYACPEVIRGEKYDGRKADVWSCGVILFALLVGALPFDDDNLRQLLEKVKRGVFHMPHFIPPDCQSLLRGMIEVDAARRLTLEHIQKHIWYIGGKNEPEPEQPIPRKVQIRSLPSLEDIDPDVLDSMHSLGCFRDRNKLLQDLLSEEENQEKMIYFLLLDRKERYPSQEDEDLPPRNEIDPPRKRVDSPMLNRHGKRRPERKSMEVLSVTDGGSPVPARRAIEMAQHGQRSRSISGASSGLSTSPLSSPRVTPHPSPRGSPLPTPKGTPVHTPKESPAGTPNPTPPSSPSVGGVPWRARLNSIKNSFLGSPRFHRRKLQVPTPEEMSNLTPESSPELAKKSWFGNFISLEKEEQIFVVIKDKPLSSIKADIVHAFLSIPSLSHSVISQTSFRAEYKATGGPAVFQKPVKFQVDITYTEGGEAQKENGIYSVTFTLLSGPSRRFKRVVETIQAQLLSTHDPPAAQHLSEPPPPAPGLSWGAGLKGQKVATSYESSL"
+        ]
