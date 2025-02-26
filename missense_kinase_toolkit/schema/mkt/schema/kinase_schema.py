c867afd5071776fc7e122bfc43afa7c30720de5e
Jess White <50890758+jessicaw9910@users.noreply.github.com>
2025-02-26 17:13:03 -0500

    Schema (#77)
    * changed schema namespace and included json files with package rather than in data directory
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * flake8 fixes in io_utils
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated schema_demo notebook for sub-package changes
    
    * updated schema demo notebook for latest changes
    
    * updated KinaseInfo json files with new field names
    
    * revised schema notebook for latest changes
    
    * now using dict to limit the possible serialization/deserialization functions; support for json, yaml, toml
    
    * using ConfigDict(use_enum_values=True) for Enum to enable serialization; default values for fields where None allowed to None; validating KLIFS2UniProt dicts to default to None if missing given toml doesn't save None entries
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * unmarked files as executable
    
    * removed unused Callable import for flake7 compatibility
    pull to merge
    
    * removed unused Optional import for flake8 compatibility
    pull to merge
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added flags to codecov.yml
    
    * added schema dependencies to test_env.yaml
    
    * schema tests
    
    * added CI yaml for schema sub-package
    
    * updated schema notebook for increased standardization and typos
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * flake8 modifications
    
    * added -e flag to pi p install - hope to resolve 'No package metadata was found' error
    
    * broke out dependencies specific to mkt.schema separately
    
    * use schema_test_env.yaml instead to try to resolve mkt-schema' requires a different Python: 3.13.2 not in '<3.12,>=3.9'
    
    * removed sub-package specific environments
    
    * conformed env and ci files to match asap
    
    * added tqdm to env and toml for schema
    
    * fixed kinhub.name to kinhub.kinase_name
    
    * added encoding='utf-8' to serialization function for Windows compatibility with TOML
    
    * added carryforwards to flags
    
    * utf-8 encoding causing Windows CI to lag - will not support Windows TOML formatting as a result
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * switched logging err
    or to info for Windows and TOML serialization
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: jessicawhite <jessicawhite@dhcp-172-30-54-80.nyguest.mskcc.org>

--- missense_kinase_toolkit/schema/mkt/schema/kinase_schema.py
+++ missense_kinase_toolkit/schema/mkt/schema/kinase_schema.py
@@ -1,7 +1,8 @@
 import logging
 from enum import Enum
+from itertools import chain
 
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, ConfigDict, constr, field_validator
 from strenum import StrEnum
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,171 @@ LIST_PFAM_KD = [
     "Protein kinase domain",
     "Protein tyrosine and serine/threonine kinase",
 ]
+"""list[str]: List of Pfam kinase domain names."""
+
+LIST_FULL_KLIFS_REGION = [
+    "I",
+    "g.l",
+    "II",
+    "II:III",
+    "III",
+    "III:αC",
+    "αC",
+    "b.l_1",
+    "b.l_intra",
+    "b.l_2",
+    "IV",
+    "IV:V",
+    "V",
+    "GK",
+    "hinge",
+    "hinge:linker",
+    "linker_1",
+    "linker_intra",
+    "linker_2",
+    "αD",
+    "αD:αE",
+    "αE",
+    "αE:VI",
+    "VI",
+    "c.l",
+    "VII",
+    "VII:VIII",
+    "VIII",
+    "xDFG",
+    "a.l",
+]
+"""list[str]: List of KLIFS region, including intra and inter regions in order."""
+
+# start/end and colors courtesy of OpenCADD
+DICT_POCKET_KLIFS_REGIONS = {
+    "I": {
+        "start": 1,
+        "end": 3,
+        "contiguous": True,
+        "color": "khaki",
+    },
+    "g.l": {
+        "start": 4,
+        "end": 9,
+        "contiguous": True,
+        "color": "green",
+    },
+    "II": {
+        "start": 10,
+        "end": 13,
+        "contiguous": True,
+        "color": "khaki",
+    },
+    "III": {
+        "start": 14,
+        "end": 19,
+        "contiguous": False,
+        "color": "khaki",
+    },
+    "αC": {
+        "start": 20,
+        "end": 30,
+        "contiguous": True,
+        "color": "red",
+    },
+    "b.l": {
+        "start": 31,
+        "end": 37,
+        "contiguous": True,
+        "color": "green",
+    },
+    "IV": {
+        "start": 38,
+        "end": 41,
+        "contiguous": False,
+        "color": "khaki",
+    },
+    "V": {
+        "start": 42,
+        "end": 44,
+        "contiguous": True,
+        "color": "khaki",
+    },
+    "GK": {
+        "start": 45,
+        "end": 45,
+        "contiguous": True,
+        "color": "orange",
+    },
+    "hinge": {
+        "start": 46,
+        "end": 48,
+        "contiguous": True,
+        "color": "magenta",
+    },
+    "linker": {
+        "start": 49,
+        "end": 52,
+        "contiguous": True,
+        "color": "cyan",
+    },
+    "αD": {
+        "start": 53,
+        "end": 59,
+        "contiguous": False,
+        "color": "red",
+    },
+    "αE": {
+        "start": 60,
+        "end": 64,
+        "contiguous": True,
+        "color": "red",
+    },
+    "VI": {
+        "start": 65,
+        "end": 67,
+        "contiguous": True,
+        "color": "khaki",
+    },
+    "c.l": {
+        "start": 68,
+        "end": 75,
+        "contiguous": True,
+        "color": "darkorange",
+    },
+    "VII": {
+        "start": 76,
+        "end": 78,
+        "contiguous": False,
+        "color": "khaki",
+    },
+    "VIII": {
+        "start": 79,
+        "end": 79,
+        "contiguous": True,
+        "color": "khaki",
+    },
+    "xDFG": {
+        "start": 80,
+        "end": 83,
+        "contiguous": True,
+        "color": "cornflowerblue",
+    },
+    "a.l": {
+        "start": 84,
+        "end": 85,
+        "contiguous": False,
+        "color": "cornflowerblue",
+    },
+}
+"""dict[str, dict[str, int | bool | str]]: Mapping KLIFS pocket region to start and end indices, \
+    boolean denoting if subsequent regions are contiguous, and colors."""
+
+LIST_KLIFS_REGION = list(
+    chain(
+        *[
+            [f"{key}:{i}" for i in range(val["start"], val["end"] + 1)]
+            for key, val in DICT_POCKET_KLIFS_REGIONS.items()
+        ]
+    )
+)
+"""list[str]: List of string of all KLIFS pocket regions in format region:idx."""
 
 
 class Group(StrEnum):
@@ -87,6 +253,8 @@ UniProtID = constr(pattern=r"^[A-Z][0-9][A-Z0-9]{3}[0-9]$")
 class KinHub(BaseModel):
     """Pydantic model for KinHub information."""
 
+    model_config = ConfigDict(use_enum_values=True)
+
     kinase_name: str
     manning_name: list[str]
     xname: list[str]
@@ -103,6 +271,8 @@ class UniProt(BaseModel):
 class KLIFS(BaseModel):
     """Pydantic model for KLIFS information."""
 
+    model_config = ConfigDict(use_enum_values=True)
+
     gene_name: str
     name: str
     full_name: str
@@ -110,11 +280,13 @@ class KLIFS(BaseModel):
     family: Family
     iuphar: int
     kinase_id: int
-    pocket_seq: SeqKLIFS | None
+    pocket_seq: SeqKLIFS | None = None
 
 
 class Pfam(BaseModel):
     """Pydantic model for Pfam information."""
+
+    model_config = ConfigDict(use_enum_values=True)
 
     domain_name: KinaseDomainName
     start: int
@@ -128,9 +300,9 @@ class KinCore(BaseModel):
     """Pydantic model for KinCore information."""
 
     seq: SeqUniProt
-    start: int | None
-    end: int | None
-    mismatch: list[int] | None
+    start: int
+    end: int
+    mismatch: list[int] | None = None
 
 
 class KinaseInfo(BaseModel):
@@ -138,10 +310,64 @@ class KinaseInfo(BaseModel):
 
     hgnc_name: str
     uniprot_id: UniProtID
-    KinHub: KinHub
-    UniProt: UniProt
-    KLIFS: KLIFS | None
-    Pfam: Pfam | None
-    KinCore: KinCore | None
+    kinhub: KinHub
+    uniprot: UniProt
+    klifs: KLIFS | None = None
+    pfam: Pfam | None = None
+    kincore: KinCore | None = None
     KLIFS2UniProtIdx: dict[str, int | None] | None = None
     KLIFS2UniProtSeq: dict[str, str | None] | None = None
+
+    @field_validator("KLIFS2UniProtIdx", mode="before")
+    @classmethod
+    def validate_klifs2uniprotidx(
+        cls,
+        value: dict[str, int | None] | None,
+    ) -> dict[str, int | None] | None:
+        """Validate KLIFS2UniProtIdx dictionary to include all regions since TOML doesn't save None.
+
+        Parameters
+        ----------
+        value : dict[str, int | None]
+            Dictionary mapping KLIFS residue to UniProt indices.
+
+        Returns
+        -------
+        dict[str, int | None]
+            Dictionary mapping KLIFS residue to UniProt indices.
+        """
+        dict_temp = dict.fromkeys(LIST_KLIFS_REGION, None)
+
+        if value is not None:
+            for key, val in value.items():
+                dict_temp[key] = val
+            return dict_temp
+        else:
+            return None
+
+    @field_validator("KLIFS2UniProtSeq", mode="before")
+    @classmethod
+    def validate_klifs2uniprotseq(
+        cls,
+        value: dict[str, str | None] | None,
+    ) -> dict[str, str | None] | None:
+        """Validate KLIFS2UniProtSeq dictionary to include all regions since TOML doesn't save None.
+
+        Parameters
+        ----------
+        value : dict[str, str | None]
+            Dictionary mapping KLIFS residue to UniProt residue.
+
+        Returns
+        -------
+        dict[str, str | None]
+            Dictionary mapping KLIFS residue to UniProt residue.
+        """
+        dict_temp = dict.fromkeys(LIST_FULL_KLIFS_REGION, None)
+
+        if value is not None:
+            for key, val in value.items():
+                dict_temp[key] = val
+            return dict_temp
+        else:
+            return None
---
+++ missense_kinase_toolkit/schema/mkt/schema/tests/test_schema.py
@@ -0,0 +1,132 @@
+from os import name
+
+
+class TestSchema:
+    def test_missense_kinase_toolkit_database_imported(self):
+        """Test if module is imported."""
+        import sys
+
+        import mkt.schema  # noqa F401
+
+        assert "mkt.schema" in sys.modules
+
+    def test_dict_kinase(self):
+        from mkt.schema import io_utils
+
+        dict_kinase = io_utils.deserialize_kinase_dict()
+        assert len(dict_kinase) == 517
+
+        # missing data
+        n_klifs = len(
+            [i.hgnc_name for i in dict_kinase.values() if i.klifs is not None]
+        )
+        assert n_klifs == 510
+
+        n_pocket = len(
+            [
+                i.hgnc_name
+                for i in dict_kinase.values()
+                if i.klifs is not None and i.klifs.pocket_seq is not None
+            ]
+        )
+        assert n_pocket == 487
+
+        n_kincore = len(
+            [i.hgnc_name for i in dict_kinase.values() if i.kincore is not None]
+        )
+        assert n_kincore == 474
+
+        n_pfam = len([i.hgnc_name for i in dict_kinase.values() if i.pfam is not None])
+        assert n_pfam == 468
+
+        n_klif2uniprot = len(
+            [
+                i.hgnc_name
+                for i in dict_kinase.values()
+                if i.KLIFS2UniProtIdx is not None
+            ]
+        )
+        assert n_klif2uniprot == 487
+
+        # check ABL1 entries
+        obj_abl1 = dict_kinase["ABL1"]
+
+        assert obj_abl1.hgnc_name == "ABL1"
+
+        assert obj_abl1.uniprot_id == "P00519"
+
+        assert obj_abl1.kinhub.kinase_name == "Tyrosine-protein kinase ABL1"
+        assert obj_abl1.kinhub.manning_name == ["ABL"]
+        assert obj_abl1.kinhub.xname == ["ABL1"]
+        assert obj_abl1.kinhub.group == ["TK"]
+        assert obj_abl1.kinhub.family == ["Other"]
+
+        assert (
+            obj_abl1.uniprot.canonical_seq
+            == "MLEICLKLVGCKSKKGLSSSSSCYLEEALQRPVASDFEPQGLSEAARWNSKENLLAGPSENDPNLFVALYDFVASGDNTLSITKGEKLRVLGYNHNGEWCEAQTKNGQGWVPSNYITPVNSLEKHSWYHGPVSRNAAEYLLSSGINGSFLVRESESSPGQRSISLRYEGRVYHYRINTASDGKLYVSSESRFNTLAELVHHHSTVADGLITTLHYPAPKRNKPTVYGVSPNYDKWEMERTDITMKHKLGGGQYGEVYEGVWKKYSLTVAVKTLKEDTMEVEEFLKEAAVMKEIKHPNLVQLLGVCTREPPFYIITEFMTYGNLLDYLRECNRQEVNAVVLLYMATQISSAMEYLEKKNFIHRDLAARNCLVGENHLVKVADFGLSRLMTGDTYTAHAGAKFPIKWTAPESLAYNKFSIKSDVWAFGVLLWEIATYGMSPYPGIDLSQVYELLEKDYRMERPEGCPEKVYELMRACWQWNPSDRPSFAEIHQAFETMFQESSISDEVEKELGKQGVRGAVSTLLQAPELPTKTRTSRRAAEHRDTTDVPEMPHSKGQGESDPLDHEPAVSPLLPRKERGPPEGGLNEDERLLPKDKKTNLFSALIKKKKKTAPTPPKRSSSFREMDGQPERRGAGEEEGRDISNGALAFTPLDTADPAKSPKPSNGAGVPNGALRESGGSGFRSPHLWKKSSTLTSSRLATGEEEGGGSSSKRFLRSCSASCVPHGAKDTEWRSVTLPRDLQSTGRQFDSSTFGGHKSEKPALPRKRAGENRSDQVTRGTVTPPPRLVKKNEEAADEVFKDIMESSPGSSPPNLTPKPLRRQVTVAPASGLPHKEEAGKGSALGTPAAAEPVTPTSKAGSGAPGGTSKGPAEESRVRRHKHSSESPGRDKGKLSRLKPAPPPPPAASAGKAGGKPSQSPSQEAAGEAVLGAKTKATSLVDAVNSDAAKPSQPGEGLKKPVLPATPKPQSAKPSGTPISPAPVPSTLPSASSALAGDQPSSTAFIPLISTRVSLRKTRQPPERIASGAITKGVVLDSTEALCLAISRNSEQMASHSAVLEAGKNLYTFCVSYVDSIQQMRNKFAFREAINKLENNLRELQICPATAGSGPAATQDFSKLLSSVKEISDIVQR"
+        )
+
+        assert obj_abl1.klifs.gene_name == "ABL1"
+        assert obj_abl1.klifs.name == "ABL1"
+        assert (
+            obj_abl1.klifs.full_name
+            == "ABL proto-oncogene 1, non-receptor tyrosine kinase"
+        )
+        assert obj_abl1.klifs.group == "TK"
+        assert obj_abl1.klifs.family == "Other"
+        assert obj_abl1.klifs.iuphar == 1923
+        assert obj_abl1.klifs.kinase_id == 392
+        assert (
+            obj_abl1.klifs.pocket_seq
+            == "HKLGGGQYGEVYEVAVKTLEFLKEAAVMKEIKPNLVQLLGVYIITEFMTYGNLLDYLREYLEKKNFIHRDLAARNCLVVADFGLS"
+        )
+
+        assert (
+            obj_abl1.pfam.domain_name == "Protein tyrosine and serine/threonine kinase"
+        )
+        assert obj_abl1.pfam.start == 242
+        assert obj_abl1.pfam.end == 492
+        assert obj_abl1.pfam.pfam_accession == "PF07714"
+        assert obj_abl1.pfam.in_alphafold is True
+
+        assert (
+            obj_abl1.kincore.seq
+            == "ITMKHKLGGGQYGEVYEGVWKKYSLTVAVKTLKEDTMEVEEFLKEAAVMKEIKHPNLVQLLGVCTREPPFYIITEFMTYGNLLDYLRECNRQEVNAVVLLYMATQISSAMEYLEKKNFIHRDLAARNCLVGENHLVKVADFGLSRLMTGDTYTAHAGAKFPIKWTAPESLAYNKFSIKSDVWAFGVLLWEIATYGMSPYPGIDLSQVYELLEKDYRMERPEGCPEKVYELMRACWQWNPSDRPSFAEIHQAFET"
+        )
+        assert obj_abl1.kincore.start == 242
+        assert obj_abl1.kincore.end == 495
+        assert obj_abl1.kincore.mismatch is None
+
+        str_dict = "".join(
+            [
+                v
+                for k, v in obj_abl1.KLIFS2UniProtSeq.items()
+                if v is not None and ":" not in k and "_intra" not in k
+            ]
+        )
+        assert obj_abl1.klifs.pocket_seq == str_dict
+
+        assert min(obj_abl1.KLIFS2UniProtIdx.values()) == 246
+        assert max(obj_abl1.KLIFS2UniProtIdx.values()) == 385
+
+    def test_serialization(self):
+        import shutil
+
+        from mkt.schema import io_utils
+
+        dict_kinase = io_utils.deserialize_kinase_dict()
+
+        for suffix in io_utils.DICT_FUNCS.keys():
+            print(f"Format: {suffix}")
+            io_utils.serialize_kinase_dict(
+                dict_kinase, suffix=suffix, str_path=f"./{suffix}"
+            )
+            if name == "nt" and suffix == "toml":
+                pass
+            else:
+                dict_temp = io_utils.deserialize_kinase_dict(
+                    suffix=suffix, str_path=f"./{suffix}"
+                )
+                assert dict_kinase == dict_temp
+                print()
+                shutil.rmtree(f"./{suffix}")
