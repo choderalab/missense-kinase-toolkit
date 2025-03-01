ff6364e9a8b4b801ec8b0890f14189bcdbd53362
jessicaw9910 <jessbwhite@gmail.com>
2025-03-01 13:24:36 -0500

    added HTTPError exception handling for KLIFS and updated tests to reflect; separated KLIFS and KinCore tests

--- missense_kinase_toolkit/databases/mkt/databases/klifs.py
+++ missense_kinase_toolkit/databases/mkt/databases/klifs.py
@@ -211,6 +211,22 @@ class KLIFS(SwaggerAPIClient):
         return self._klifs
 
 
+LIST_KINASEINFO_KEYS = [
+    "family",
+    "full_name",
+    "gene_name",
+    "group",
+    "iuphar",
+    "kinase_ID",
+    "name",
+    "pocket",
+    "species",
+    "subfamily",
+    "uniprot",
+]
+"""list[str]: List of keys for kinase information; use to populate if 404 error."""
+
+
 class KinaseInfo(KLIFS):
     """Class to get information about a kinase from KLIFS."""
 
@@ -243,6 +259,8 @@ class KinaseInfo(KLIFS):
             only used to post-hoc annotate column with search term in case of missing data
         species : str
             Species of the kinase
+        status_code : int | None
+            Status code of the query; None if query fails
         _kinase_info : dict[str, str | int | None]
             KLIFS API object for search term
 
@@ -251,6 +269,7 @@ class KinaseInfo(KLIFS):
         self.search_term = search_term
         self.search_field = search_field
         self.species = species
+        self.status_code = 200
         self._kinase_info = self.query_kinase_info()
 
     def query_kinase_info(self) -> dict[str, str | int | None]:
@@ -270,34 +289,44 @@ class KinaseInfo(KLIFS):
                 .response()
                 .result[0]
             )
-
+        except Exception as e:
+            if hasattr(e, "status_code"):
+                # 404 error: kinase not found in KLIFS
+                if e.status_code == 404:
+                    print(
+                        f"Kinase {self.search_term} "
+                        f"(field: {self.search_field} not found in KLIFS"
+                    )
+                    dict_kinase_info = self.handle_page_not_found()
+                    self.status_code = 404
+                # 500 error: server error
+                elif 500 <= e.status_code < 600:
+                    print(f"Server error: {e.status_code}")
+                    self.status_code = e.status_code
+                    dict_kinase_info = None
+                # all other errors (e.g., 400)
+                else:
+                    print(
+                        f"Error {e} in query_kinase_info for "
+                        f"{self.search_term} (field: {self.search_field})"
+                    )
+                    self.status_code = e.status_code
+                    dict_kinase_info = None
+            else:
+                print(f"Error {e} in query_kinase_info for {self.search_term}")
+                self.status_code = None
+        if self.status_code == 200:
             list_key = dir(kinase_info)
             list_val = [getattr(kinase_info, key) for key in list_key]
-
             dict_kinase_info = dict(zip(list_key, list_val))
 
-        except Exception as e:
-            print(
-                f"Error in query_kinase_info for {self.search_term} (field: {self.search_field}):"
-            )
-            print(e)
-            list_key = [
-                "family",
-                "full_name",
-                "gene_name",
-                "group",
-                "iuphar",
-                "kinase_ID",
-                "name",
-                "pocket",
-                "species",
-                "subfamily",
-                "uniprot",
-            ]
-            dict_kinase_info = dict(zip(list_key, [None] * len(list_key)))
-            if self.search_field is not None:
-                dict_kinase_info[self.search_field] = self.search_term
+        return dict_kinase_info
 
+    def handle_page_not_found(self):
+        """Handle page not found error."""
+        dict_kinase_info = dict(zip(LIST_KINASEINFO_KEYS, [None] * len(LIST_KINASEINFO_KEYS)))
+        if self.search_field is not None:
+            dict_kinase_info[self.search_field] = self.search_term
         return dict_kinase_info
 
     def get_search_term(self):
@@ -311,6 +340,10 @@ class KinaseInfo(KLIFS):
     def get_species(self):
         """Get species used for query."""
         return self.species
+
+    def get_status_code(self):
+        """Get status code of the query."""
+        return self.status_code
 
     def get_kinase_info(self):
         """Get information about the kinase from KLIFS query."""
--- missense_kinase_toolkit/databases/mkt/databases/tests/test_databases.py
+++ missense_kinase_toolkit/databases/mkt/databases/tests/test_databases.py
@@ -170,36 +170,216 @@ class TestDatabases:
         )
         assert test.hgnc == "ABL1"
 
-    def test_klifs_kincore(self):
+    def test_klifs(self):
         from mkt.databases import klifs
+        from mkt.databases.uniprot import UniProt
+
+        temp_obj = klifs.KinaseInfo("EGFR")
+        dict_egfr = temp_obj._kinase_info
+        if temp_obj.status_code == 200:
+            assert dict_egfr["family"] == "EGFR"
+            assert dict_egfr["full_name"] == "epidermal growth factor receptor"
+            assert dict_egfr["gene_name"] == "EGFR"
+            assert dict_egfr["group"] == "TK"
+            assert dict_egfr["iuphar"] == 1797
+            assert dict_egfr["kinase_ID"] == 406
+            assert dict_egfr["name"] == "EGFR"
+            assert (
+                dict_egfr["pocket"]
+                == "KVLGSGAFGTVYKVAIKELEILDEAYVMASVDPHVCRLLGIQLITQLMPFGCLLDYVREYLEDRRLVHRDLAARNVLVITDFGLA"
+            )
+            assert dict_egfr["species"] == "Human"
+            assert dict_egfr["uniprot"] == "P00533"
+
+            egfr_uniprot = UniProt(dict_egfr["uniprot"])
+
+            # check KLIFS pocket alignment to UniProt sequence
+            egfr_pocket = klifs.KLIFSPocket(
+                uniprotSeq=egfr_uniprot._sequence,
+                klifsSeq=dict_egfr["pocket"],
+                idx_kd=(egfr_align["start"] - 1, egfr_align["end"] - 1),
+            )
+
+            assert egfr_pocket.list_klifs_substr_actual == [
+                "KVL",
+                "GSGAFG",
+                "TVYK",
+                "VAIKEL",
+                "EILDEAYVMAS",
+                "VDPHVCR",
+                "LLGI",
+                "QLI",
+                "T",
+                "QLM",
+                "PFGC",
+                "LLDYVRE",
+                "YLEDR",
+                "RLV",
+                "HRDLAARN",
+                "VLV",
+                "I",
+                "TDFG",
+                "LA",
+            ]
+            assert egfr_pocket.list_klifs_substr_match == [
+                "KVL",
+                "GSGAFG",
+                "TVYK",
+                "VAIKEL",
+                "EILDEAYVMAS",
+                "VDPHVCR",
+                "LLGI",
+                "QLI",
+                "TQLM",
+                "QLM",
+                "PFGC",
+                "LLDYVRE",
+                "YLEDR",
+                "RLV",
+                "HRDLAARN",
+                "VLV",
+                "ITDFG",
+                "TDFG",
+                "TDFGLA",
+            ]
+            assert egfr_pocket.KLIFS2UniProtIdx == {
+                "I:1": 716,
+                "I:2": 717,
+                "I:3": 718,
+                "g.l:4": 719,
+                "g.l:5": 720,
+                "g.l:6": 721,
+                "g.l:7": 722,
+                "g.l:8": 723,
+                "g.l:9": 724,
+                "II:10": 725,
+                "II:11": 726,
+                "II:12": 727,
+                "II:13": 728,
+                "III:14": 742,
+                "III:15": 743,
+                "III:16": 744,
+                "III:17": 745,
+                "III:18": 746,
+                "III:19": 747,
+                "αC:20": 758,
+                "αC:21": 759,
+                "αC:22": 760,
+                "αC:23": 761,
+                "αC:24": 762,
+                "αC:25": 763,
+                "αC:26": 764,
+                "αC:27": 765,
+                "αC:28": 766,
+                "αC:29": 767,
+                "αC:30": 768,
+                "b.l:31": 769,
+                "b.l:32": 770,
+                "b.l:33": 772,
+                "b.l:34": 773,
+                "b.l:35": 774,
+                "b.l:36": 775,
+                "b.l:37": 776,
+                "IV:38": 777,
+                "IV:39": 778,
+                "IV:40": 779,
+                "IV:41": 780,
+                "V:42": 787,
+                "V:43": 788,
+                "V:44": 789,
+                "GK:45": 790,
+                "hinge:46": 791,
+                "hinge:47": 792,
+                "hinge:48": 793,
+                "linker:49": 794,
+                "linker:50": 795,
+                "linker:51": 796,
+                "linker:52": 797,
+                "αD:53": 798,
+                "αD:54": 799,
+                "αD:55": 800,
+                "αD:56": 801,
+                "αD:57": 802,
+                "αD:58": 803,
+                "αD:59": 804,
+                "αE:60": 827,
+                "αE:61": 828,
+                "αE:62": 829,
+                "αE:63": 830,
+                "αE:64": 831,
+                "VI:65": 832,
+                "VI:66": 833,
+                "VI:67": 834,
+                "c.l:68": 835,
+                "c.l:69": 836,
+                "c.l:70": 837,
+                "c.l:71": 838,
+                "c.l:72": 839,
+                "c.l:73": 840,
+                "c.l:74": 841,
+                "c.l:75": 842,
+                "VII:76": 843,
+                "VII:77": 844,
+                "VII:78": 845,
+                "VIII:79": 853,
+                "xDFG:80": 854,
+                "xDFG:81": 855,
+                "xDFG:82": 856,
+                "xDFG:83": 857,
+                "a.l:84": 858,
+                "a.l:85": 859,
+            }
+
+            assert egfr_pocket.KLIFS2UniProtSeq == {
+                "I": "KVL",
+                "g.l": "GSGAFG",
+                "II": "TVYK",
+                "II:III": "GLWIPEGEKVKIP",
+                "III": "VAIKEL",
+                "III:αC": "REATSPKANK",
+                "αC": "EILDEAYVMAS",
+                "b.l_1": "VD",
+                "b.l_intra": "N",
+                "b.l_2": "PHVCR",
+                "IV": "LLGI",
+                "IV:V": "CLTSTV",
+                "V": "QLI",
+                "GK": "T",
+                "hinge": "QLM",
+                "hinge:linker": None,
+                "linker_1": "P",
+                "linker_intra": None,
+                "linker_2": "FGC",
+                "αD": "LLDYVRE",
+                "αD:αE": "HKDNIGSQYLLNWCVQIAKGMN",
+                "αE": "YLEDR",
+                "αE:VI": None,
+                "VI": "RLV",
+                "c.l": "HRDLAARN",
+                "VII": "VLV",
+                "VII:VIII": "KTPQHVK",
+                "VIII": "I",
+                "xDFG": "TDFG",
+                "a.l": "LA",
+            }
+
+        if 500 <= temp_obj.status_code < 600:
+            assert dict_egfr is None
+
+
+    def test_kincore(self):
         from mkt.databases.kincore import (
             align_kincore2uniprot,
             extract_pk_fasta_info_as_dict,
         )
         from mkt.databases.uniprot import UniProt
 
-        dict_egfr = klifs.KinaseInfo("EGFR")._kinase_info
-
-        assert dict_egfr["family"] == "EGFR"
-        assert dict_egfr["full_name"] == "epidermal growth factor receptor"
-        assert dict_egfr["gene_name"] == "EGFR"
-        assert dict_egfr["group"] == "TK"
-        assert dict_egfr["iuphar"] == 1797
-        assert dict_egfr["kinase_ID"] == 406
-        assert dict_egfr["name"] == "EGFR"
-        assert (
-            dict_egfr["pocket"]
-            == "KVLGSGAFGTVYKVAIKELEILDEAYVMASVDPHVCRLLGIQLITQLMPFGCLLDYVREYLEDRRLVHRDLAARNVLVITDFGLA"
-        )
-        assert dict_egfr["species"] == "Human"
-        assert dict_egfr["uniprot"] == "P00533"
-
+        uniprot_id = "P00533"
+        egfr_uniprot = UniProt(uniprot_id)
         dict_kincore = extract_pk_fasta_info_as_dict()
 
-        egfr_uniprot = UniProt(dict_egfr["uniprot"])
-
         egfr_align = align_kincore2uniprot(
-            str_kincore=dict_kincore[dict_egfr["uniprot"]]["seq"],
+            str_kincore=dict_kincore[uniprot_id]["seq"],
             str_uniprot=egfr_uniprot._sequence,
         )
 
@@ -211,173 +391,6 @@ class TestDatabases:
         assert egfr_align["end"] == 970
         assert egfr_align["mismatch"] is None
 
-        egfr_pocket = klifs.KLIFSPocket(
-            uniprotSeq=egfr_uniprot._sequence,
-            klifsSeq=dict_egfr["pocket"],
-            idx_kd=(egfr_align["start"] - 1, egfr_align["end"] - 1),
-        )
-
-        assert egfr_pocket.list_klifs_substr_actual == [
-            "KVL",
-            "GSGAFG",
-            "TVYK",
-            "VAIKEL",
-            "EILDEAYVMAS",
-            "VDPHVCR",
-            "LLGI",
-            "QLI",
-            "T",
-            "QLM",
-            "PFGC",
-            "LLDYVRE",
-            "YLEDR",
-            "RLV",
-            "HRDLAARN",
-            "VLV",
-            "I",
-            "TDFG",
-            "LA",
-        ]
-        assert egfr_pocket.list_klifs_substr_match == [
-            "KVL",
-            "GSGAFG",
-            "TVYK",
-            "VAIKEL",
-            "EILDEAYVMAS",
-            "VDPHVCR",
-            "LLGI",
-            "QLI",
-            "TQLM",
-            "QLM",
-            "PFGC",
-            "LLDYVRE",
-            "YLEDR",
-            "RLV",
-            "HRDLAARN",
-            "VLV",
-            "ITDFG",
-            "TDFG",
-            "TDFGLA",
-        ]
-        assert egfr_pocket.KLIFS2UniProtIdx == {
-            "I:1": 716,
-            "I:2": 717,
-            "I:3": 718,
-            "g.l:4": 719,
-            "g.l:5": 720,
-            "g.l:6": 721,
-            "g.l:7": 722,
-            "g.l:8": 723,
-            "g.l:9": 724,
-            "II:10": 725,
-            "II:11": 726,
-            "II:12": 727,
-            "II:13": 728,
-            "III:14": 742,
-            "III:15": 743,
-            "III:16": 744,
-            "III:17": 745,
-            "III:18": 746,
-            "III:19": 747,
-            "αC:20": 758,
-            "αC:21": 759,
-            "αC:22": 760,
-            "αC:23": 761,
-            "αC:24": 762,
-            "αC:25": 763,
-            "αC:26": 764,
-            "αC:27": 765,
-            "αC:28": 766,
-            "αC:29": 767,
-            "αC:30": 768,
-            "b.l:31": 769,
-            "b.l:32": 770,
-            "b.l:33": 772,
-            "b.l:34": 773,
-            "b.l:35": 774,
-            "b.l:36": 775,
-            "b.l:37": 776,
-            "IV:38": 777,
-            "IV:39": 778,
-            "IV:40": 779,
-            "IV:41": 780,
-            "V:42": 787,
-            "V:43": 788,
-            "V:44": 789,
-            "GK:45": 790,
-            "hinge:46": 791,
-            "hinge:47": 792,
-            "hinge:48": 793,
-            "linker:49": 794,
-            "linker:50": 795,
-            "linker:51": 796,
-            "linker:52": 797,
-            "αD:53": 798,
-            "αD:54": 799,
-            "αD:55": 800,
-            "αD:56": 801,
-            "αD:57": 802,
-            "αD:58": 803,
-            "αD:59": 804,
-            "αE:60": 827,
-            "αE:61": 828,
-            "αE:62": 829,
-            "αE:63": 830,
-            "αE:64": 831,
-            "VI:65": 832,
-            "VI:66": 833,
-            "VI:67": 834,
-            "c.l:68": 835,
-            "c.l:69": 836,
-            "c.l:70": 837,
-            "c.l:71": 838,
-            "c.l:72": 839,
-            "c.l:73": 840,
-            "c.l:74": 841,
-            "c.l:75": 842,
-            "VII:76": 843,
-            "VII:77": 844,
-            "VII:78": 845,
-            "VIII:79": 853,
-            "xDFG:80": 854,
-            "xDFG:81": 855,
-            "xDFG:82": 856,
-            "xDFG:83": 857,
-            "a.l:84": 858,
-            "a.l:85": 859,
-        }
-        assert egfr_pocket.KLIFS2UniProtSeq == {
-            "I": "KVL",
-            "g.l": "GSGAFG",
-            "II": "TVYK",
-            "II:III": "GLWIPEGEKVKIP",
-            "III": "VAIKEL",
-            "III:αC": "REATSPKANK",
-            "αC": "EILDEAYVMAS",
-            "b.l_1": "VD",
-            "b.l_intra": "N",
-            "b.l_2": "PHVCR",
-            "IV": "LLGI",
-            "IV:V": "CLTSTV",
-            "V": "QLI",
-            "GK": "T",
-            "hinge": "QLM",
-            "hinge:linker": None,
-            "linker_1": "P",
-            "linker_intra": None,
-            "linker_2": "FGC",
-            "αD": "LLDYVRE",
-            "αD:αE": "HKDNIGSQYLLNWCVQIAKGMN",
-            "αE": "YLEDR",
-            "αE:VI": None,
-            "VI": "RLV",
-            "c.l": "HRDLAARN",
-            "VII": "VLV",
-            "VII:VIII": "KTPQHVK",
-            "VIII": "I",
-            "xDFG": "TDFG",
-            "a.l": "LA",
-        }
 
     def test_scrapers(self):
         from mkt.databases import scrapers
