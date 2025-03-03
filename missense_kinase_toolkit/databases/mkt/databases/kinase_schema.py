5036052eadcc51d5ce5febc30ae68c375ad097ad
jessicaw9910 <jessbwhite@gmail.com>
2025-03-03 12:02:04 -0500

    made mkt.databases a dependency of mkt.schema; databases now has a KinaseInfoGenerator that inherits from KinaseInfo

--- missense_kinase_toolkit/databases/mkt/databases/kinase_schema.py
+++ missense_kinase_toolkit/databases/mkt/databases/kinase_schema.py
@@ -8,151 +8,26 @@ from mkt.databases import klifs
 from mkt.databases.aligners import ClustalOmegaAligner
 from mkt.databases.kincore import align_kincore2uniprot, extract_pk_fasta_info_as_dict
 from mkt.databases.utils import get_repo_root
+from mkt.schema.constants import LIST_PFAM_KD
+from mkt.schema.kinase_schema import (
+    KinaseInfo,
+    Group,
+    Family,
+    KinHub,
+    UniProt,
+    KLIFS,
+    Pfam,
+    KinCore,
+)
 from pydantic import BaseModel, ValidationError, constr, model_validator
 from typing_extensions import Self
 
 logger = logging.getLogger(__name__)
 
 
-LIST_PFAM_KD = [
-    "Protein kinase domain",
-    "Protein tyrosine and serine/threonine kinase",
-]
-
-
-class Group(str, Enum):
-    """Enum class for kinase groups."""
-
-    AGC = "AGC"  # Protein Kinase A, G, and C families
-    Atypical = "Atypical"  # Atypical protein kinases
-    CAMK = "CAMK"  # Calcium/calmodulin-dependent protein kinase family
-    CK1 = "CK1"  # Casein kinase 1 family
-    CMGC = "CMGC"  # Cyclin-dependent kinase, Mitogen-activated protein kinase, Glycogen synthase kinase, and CDK-like kinase families
-    RGC = "RGC"  # Receptor guanylate cyclase family
-    STE = "STE"  # Homologs of yeast Sterile 7, Sterile 11, Sterile 20 kinases
-    TK = "TK"  # Tyrosine kinase family
-    TKL = "TKL"  # Tyrosine kinase-like family
-    Other = "Other"  # Other protein kinases
-
-
-class Family(Enum):
-    """Enum class for kinase families (>=5 in KinHub)."""
-
-    STE20 = "STE20"
-    CAMKL = "CAMKL"
-    CDK = "CDK"
-    Eph = "Eph"
-    MAPK = "MAPK"
-    STKR = "STKR"
-    NEK = "NEK"
-    Src = "Src"
-    DYRK = "DYRK"
-    PKC = "PKC"
-    STE11 = "STE11"
-    RSK = "RSK"
-    MLK = "MLK"
-    GRK = "GRK"
-    CK1 = "CK1"
-    DMPK = "DMPK"
-    STE7 = "STE7"
-    PIKK = "PIKK"
-    RSKb = "RSKb"
-    Alpha = "Alpha"
-    Tec = "Tec"
-    CAMK1 = "CAMK1"
-    PDGFR = "PDGFR"
-    ULK = "ULK"
-    DAPK = "DAPK"
-    RAF = "RAF"
-    RIPK = "RIPK"
-    MLCK = "MLCK"
-    PKA = "PKA"
-    MAPKAPK = "MAPKAPK"
-    RGC = "RGC"
-    CDKL = "CDKL"
-    MAST = "MAST"
-    TSSK = "TSSK"
-    ABC1 = "ABC1"
-    PDHK = "PDHK"
-    Jak = "Jak"
-    Jakb = "Jakb"
-    Other = "Other"
-    Null = None
-
-
-KinaseDomainName = StrEnum(
-    "KinaseDomainName", {"KD" + str(idx + 1): kd for idx, kd in enumerate(LIST_PFAM_KD)}
-)
-SeqUniProt = constr(pattern=r"^[ACDEFGHIKLMNPQRSTVWXY]+$")
-"""Pydantic model for UniProt sequence constraints."""
-SeqKLIFS = constr(pattern=r"^[ACDEFGHIKLMNPQRSTVWY\-]{85}$")
-"""Pydantic model for KLIFS pocket sequence constraints."""
-UniProtID = constr(pattern=r"^[A-Z][0-9][A-Z0-9]{3}[0-9]$")
-"""Pydantic model for UniProt ID constraints."""
-
-
-class KinHub(BaseModel):
-    """Pydantic model for KinHub information."""
-
-    kinase_name: str
-    manning_name: list[str]
-    xname: list[str]
-    group: list[Group]
-    family: list[Family]
-
-
-class UniProt(BaseModel):
-    """Pydantic model for UniProt information."""
-
-    canonical_seq: SeqUniProt
-
-
-class KLIFS(BaseModel):
-    """Pydantic model for KLIFS information."""
-
-    gene_name: str
-    name: str
-    full_name: str
-    group: Group
-    family: Family
-    iuphar: int
-    kinase_id: int
-    pocket_seq: SeqKLIFS | None
-
-
-class Pfam(BaseModel):
-    """Pydantic model for Pfam information."""
-
-    domain_name: KinaseDomainName
-    start: int
-    end: int
-    protein_length: int
-    pfam_accession: str
-    in_alphafold: bool
-
-
-class KinCore(BaseModel):
-    """Pydantic model for KinCore information."""
-
-    seq: SeqUniProt
-    start: int | None
-    end: int | None
-    mismatch: list[int] | None
-
-
-# TODO: make mkt.schema a dependency in pyproject.toml and turn this into "KinaseInfoGenerator"
-class KinaseInfo(BaseModel):
+class KinaseInfoGenerator(KinaseInfo):
     """Pydantic model for kinase information."""
 
-    hgnc_name: str
-    uniprot_id: UniProtID
-    kinhub: KinHub
-    uniprot: UniProt
-    klifs: KLIFS | None = None
-    pfam: Pfam | None = None
-    kincore: KinCore | None = None
-    KLIFS2UniProtIdx: dict[str, int | None] | None = None
-    KLIFS2UniProtSeq: dict[str, str | None] | None = None
     bool_offset: bool = True
 
     # https://docs.pydantic.dev/latest/examples/custom_validators/#validating-nested-model-fields
@@ -508,7 +383,7 @@ def create_kinase_models_from_df(
             kincore_model = None
 
         # create KinaseInfo model
-        kinase_info = KinaseInfo(
+        kinase_info = KinaseInfoGenerator(
             hgnc_name=name_hgnc,
             uniprot_id=id_uniprot,
             kinhub=kinhub_model,
