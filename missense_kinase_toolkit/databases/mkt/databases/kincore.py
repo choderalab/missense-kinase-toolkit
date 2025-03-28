046580a656a84e4e0676765f820f8a68fcfbfb6c
jessicaw9910 <jessbwhite@gmail.com>
2025-03-28 14:10:45 -0400

    processes CIF files and all filename/header metadata; incorporates KD fastas from both Modi and Faezov papers

--- missense_kinase_toolkit/databases/mkt/databases/kincore.py
+++ missense_kinase_toolkit/databases/mkt/databases/kincore.py
@@ -1,44 +1,237 @@
+import glob
+import logging
 import os
 import re
+from collections import Counter
+from itertools import chain
 
 from Bio import SeqIO
 from mkt.databases.aligners import Kincore2UniProtAligner
-from mkt.databases.io_utils import get_repo_root
+from mkt.databases.utils import (
+    flatten_iterables_in_iterable,
+    split_on_first_only,
+    try_except_split_concat_str,
+)
+from mkt.schema.io_utils import return_str_path_from_pkg_data
+from mkt.schema.kinase_schema import KinCore, KinCoreCIF, KinCoreFASTA
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
-def extract_pk_fasta_info_as_dict(
-    str_filename: str = "Human-PK.fasta",
-) -> dict[str, dict[str, str | int]]:
+def return_fasta_contents(path_filename=str) -> SeqIO.FastaIO.FastaIterator:
+    return SeqIO.parse(open(path_filename), "fasta")
+
+
+LIST_FASTA_KEYS1 = [
+    "seq",
+    "group",
+    "hgnc1",
+    "swissprot",
+    "hgnc2",
+    "uniprot",
+    "start_md",
+    "end_md",
+    "length_md",
+    "start_af2",
+    "end_af2",
+    "length_af2",
+    "length_uniprot",
+    "source_file",
+]
+
+
+LIST_FASTA_KEYS2 = [
+    "seq",
+    "group",
+    "hgnc1",
+    "start_md",
+    "end_md",
+    "swissprot",
+    "hgnc2",
+    "uniprot",
+    "source_file",
+]
+
+
+DICT_KINCORE_PARAMS = {
+    "af2": {
+        "filename": "AF2-active.fasta",
+        "LIST_FASTA_KEYS": LIST_FASTA_KEYS1,
+        "bool_af2": True,
+        "study": "Faezov-Dunbrack_2023",
+    },
+    "md": {
+        "filename": "Human-PK.fasta",
+        "LIST_FASTA_KEYS": LIST_FASTA_KEYS2,
+        "bool_af2": False,
+        "study": "Modi-Dunbrack_2019",
+    },
+}
+
+
+DICT_GROUP_KINCORE = {
+    "AGC": "AGC",
+    "CAMK": "CAMK",
+    "CK1": "CK1",
+    "CMGC": "CMGC",
+    "NEK": "NEK",
+    "OTHER": "Other",
+    "RGC": "RGC",  # this is only in Modi-Dunbrack dataset, not AF2
+    "STE": "STE",
+    "TKL": "TKL",
+    "TYR": "TK",
+}
+
+
+def parse_fasta_description(
+    str_description: str,
+    bool_af2: bool = True,
+) -> dict[str, str | int]:
+    """Parse fasta description to extract metadata.
+
+    Parameters
+    ----------
+    str_description : str
+        Description from fasta file
+
+    Returns
+    -------
+    dict[str, str]
+        Dictionary of metadata
+    """
+    if bool_af2:
+        # remove extra spaces only present in AF2-active headers
+        str_description = " ".join(str_description.split())
+
+    temp = str_description.split(" ")
+
+    for char in ["/", "-"]:
+        temp = list(chain(*[i.split(char) for i in temp]))
+
+    temp = [
+        split_on_first_only(i, "_") if idx == 0 else i for idx, i in enumerate(temp)
+    ]
+    temp = flatten_iterables_in_iterable(temp)
+
+    return temp
+
+
+def extract_pk_fasta_info_as_list(
+    study: str,
+) -> list[KinCoreFASTA]:
     """Parse KinCore Human-PK.fasta file to extract information for KinaseInfo object.
 
     Parameters
     ----------
-    str_filename : str, optional
-        Filename of the fasta file, by default "Human-PK.fasta"
+    study : str
+        Study FASTA to use; options are "af2" (Faezov-Dunbrack, 2023) or "md" (Modi-Dunbrack, 2019)
 
     Returns
     -------
-    dict[str, dict[str, str | int]]
-        Dictionary of {uniprot : {seq : str, start : int, end : int}}
+    list[KinCoreFASTA]
+        List of KinCoreFASTA objects
     """
-    fasta_sequences = SeqIO.parse(
-        open(os.path.join(get_repo_root(), "data", str_filename)), "fasta"
+    try:
+        dict_temp = DICT_KINCORE_PARAMS[study]
+        str_filename = dict_temp["filename"]
+        list_fasta_keys = dict_temp["LIST_FASTA_KEYS"]
+        bool_af2 = dict_temp["bool_af2"]
+        study = dict_temp["study"]
+    except KeyError:
+        logger.error(f"Study {study} not recognized; must be 'af2' or 'md'")
+        return None
+
+    str_path = return_str_path_from_pkg_data(
+        pkg_name="mkt.databases", pkg_resource="KinCore"
     )
-    list_description, list_seq = [], []
-    for fasta in fasta_sequences:
-        list_description.append(fasta.description)
-        list_seq.append(str(fasta.seq))
 
-    list_uniprot = [x.split(" ")[-1] for x in list_description]
+    str_path_filename = os.path.join(str_path, str_filename)
+    if not os.path.exists(str_path_filename):
+        logger.error(f"File {str_path_filename} does not exist")
 
-    dict_out = {
-        list_uniprot[i]: {
-            "seq": list_seq[i],
-        }
-        for i in range(len(list_uniprot))
-    }
+    fasta_sequences = return_fasta_contents(str_path_filename)
+    list_out = [
+        dict(
+            zip(
+                list_fasta_keys,
+                [str(fasta.seq)]
+                + parse_fasta_description(fasta.description, bool_af2)
+                + [study],
+            )
+        )
+        for fasta in fasta_sequences
+    ]
 
-    return dict_out
+    for i in list_out:
+        for k, v in i.items():
+            if "start" in k or "end" in k or "length" in k:
+                i[k] = int(v)
+        i["group"] = DICT_GROUP_KINCORE[i["group"]]
+        i["hgnc"] = {i["hgnc1"], i["hgnc2"]}
+        i.pop("hgnc1")
+        i.pop("hgnc2")
+
+    list_out = [KinCoreFASTA.model_validate(i) for i in list_out]
+
+    return list_out
+
+
+LIST_CIF_KEYS = [
+    "cif",
+    "group",
+    "hgnc",
+    "min_aloop_pLDDT",
+    "template_source",
+    "msa_size",
+    "msa_source",
+    "model_no",
+]
+
+
+def extract_pk_cif_files_as_list() -> list[KinCoreCIF]:
+    """Extract all cif files from KinCore directory.
+
+    Returns
+    -------
+    list[KinCoreCIF]
+        List of KinCoreCIF objects
+    """
+    # from biotite.structure.io.pdbx import CIFFile
+    from Bio.PDB.MMCIF2Dict import MMCIF2Dict
+
+    str_path = return_str_path_from_pkg_data(
+        pkg_name="mkt.databases", pkg_resource="KinCore"
+    )
+    list_file = glob.glob(os.path.join(str_path, "*.cif"))
+
+    list_out = []
+    for file in tqdm(list_file, desc="Extracting and processing CIF files..."):
+        # use the filename to extract metadata as dict
+        filename = os.path.basename(file)
+        if filename == "TYR_LMTK2_38.37_tea2MSA_AF2tholog_model1.cif":
+            # TODO: confirm this is the correct filename with Dunbrack lab
+            filename = "TYR_LMTK2_38.37_activeAF2_2MSA_ortholog_model1.cif"
+        list_filename = filename.replace(".cif", "").replace("__", "_").split("_")
+        # cif_file = CIFFile.read(file)
+        dict_temp = dict(
+            zip(
+                LIST_CIF_KEYS,
+                # [cif_file.serialize()] + list_filename
+                [MMCIF2Dict(file)] + list_filename,
+            )
+        )
+        list_out.append(dict_temp)
+
+    for v in list_out:
+        v["group"] = DICT_GROUP_KINCORE[v["group"]]
+        v["min_aloop_pLDDT"] = float(v["min_aloop_pLDDT"])
+        v["msa_size"] = int(v["msa_size"].replace("MSA", ""))
+        v["model_no"] = int(v["model_no"].replace("model", ""))
+
+    list_out = [KinCoreCIF.model_validate(v) for v in list_out]
+
+    return list_out
 
 
 def align_kincore2uniprot(
@@ -100,3 +293,67 @@ def align_kincore2uniprot(
         dict_out["mismatch"] = [idx for idx, i in enumerate(str_align) if i == "."]
 
     return dict_out
+
+
+def harmonize_kincore_fasta_cif():
+    """Harmonize KinCore FASTA/CIF files for af2/md and generate KinCore objects.
+
+    Returns
+    -------
+    dict[str, list[KinCore]]
+        Dictionary of {uniprot : list[KinCore]}
+    """
+    list_af2_fasta = extract_pk_fasta_info_as_list("af2")
+    list_md_fasta = extract_pk_fasta_info_as_list("md")
+    list_kincore_cif = extract_pk_cif_files_as_list()
+
+    dict_kincore = {}
+
+    # process AF2-active dataset
+    list_af2_uniprot = [i.uniprot for i in list_af2_fasta]
+    list_cif_hgnc_split = [
+        try_except_split_concat_str(i.hgnc, idx1=0, idx2=1) for i in list_kincore_cif
+    ]
+    # multi-kinase domain (AF2)
+    list_multi = [
+        item for item, count in Counter(list_af2_uniprot).items() if count > 1
+    ]
+    for uniprot in list_multi:
+        fastas = [i for i in list_af2_fasta if i.uniprot == uniprot]
+        list_temp = []
+        for fasta in fastas:
+            hgnc_fasta = max(fasta.hgnc, key=len)
+            idx = list_cif_hgnc_split.index(hgnc_fasta)
+            cif = list_kincore_cif[idx]
+            list_temp.append(KinCore(fasta=fasta, cif=cif))
+        dict_kincore[uniprot] = list_temp
+    # single kinase domain (AF2)
+    for uniprot in list_af2_uniprot:
+        fasta = [i for i in list_af2_fasta if i.uniprot == uniprot]
+        # don't re-incorporate multi-mapping
+        if len(fasta) == 1:
+            hgnc = fasta[0].hgnc  # use whole set for CILK1/ILK
+            try:
+                idx = [idx for idx, i in enumerate(list_cif_hgnc_split) if i in hgnc][0]
+                cif = list_kincore_cif[idx]
+                temp = KinCore(fasta=fasta[0], cif=cif)
+            except IndexError:
+                temp = KinCore(fasta=fasta[0], cif=None)
+            dict_kincore[uniprot] = [temp]
+
+    # process Modi-Dunbrack dataset
+    list_md_only_uniprot = [
+        i.uniprot for i in list_md_fasta if i.uniprot not in list_af2_uniprot
+    ]
+    # MD genes only - there are no multi-KD
+    for uniprot in list_md_only_uniprot:
+        fasta = [i for i in list_md_fasta if i.uniprot == uniprot]
+        if len(fasta) == 1:
+            temp = KinCore(fasta=fasta[0], cif=None)
+        else:
+            logger.warning(
+                f"{uniprot} has multipe FASTA entries in Modi-Dunbrack dataset\n{fasta}\n"
+            )
+        dict_kincore[uniprot] = [temp]
+
+    return dict_kincore
