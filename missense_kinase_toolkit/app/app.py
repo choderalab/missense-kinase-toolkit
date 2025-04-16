b514ea14bc2c2a8141d44479bda25672fa5108d3
jessicaw9910 <jessbwhite@gmail.com>
2025-04-16 09:16:25 -0400

    added docstrings for hardcoded resources; added KLIFS annotation to describe stick regions

--- missense_kinase_toolkit/app/app.py
+++ missense_kinase_toolkit/app/app.py
@@ -22,6 +22,7 @@ DICT_RESOURCE_URLS = {
     "UniProt": "https://www.uniprot.org/",
     "Pfam": "https://www.ebi.ac.uk/interpro/entry/pfam",
 }
+"""dict[str, str]: Dictionary containing the resource URLs for the dashboard."""
 
 LIST_OPTIONS = [
     "None",
@@ -29,13 +30,15 @@ LIST_OPTIONS = [
     "KLIFS",
     "Mutational density",
 ]
+"""list[str]: List of structure options for the dashboard."""
 
 LIST_CAPTIONS = [
     "No additional annotation",
     "Phosphorylation sites as adjudicated by UniProt",
-    "Residues that belong to the KLIFS binding pocket",
+    "Residues that belong to the KLIFS binding pocket (hinge, HRD, xDFG regions represented as sticks)",
     "Missense mutational density within cBioPortal MSK-IMPACT cohort ([Zehir et al, 2017.](https://www.nature.com/articles/nm.4333))",
 ]
+"""list[str]: List of captions for the structure options in the dashboard."""
 
 
 @dataclass
