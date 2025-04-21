a6f767a559e88d6091ed46376108417755ab3ca2
jessicaw9910 <jessbwhite@gmail.com>
2025-04-21 17:08:42 -0400

    added get_repo_root to utils

--- missense_kinase_toolkit/ml/mkt/ml/utils.py
+++ missense_kinase_toolkit/ml/mkt/ml/utils.py
@@ -1,4 +1,24 @@
+import git
 import torch
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_repo_root():
+    """Get the root of the git repository.
+
+    Returns
+    -------
+    str
+        Path to the root of the git repository; if not found, return current directory
+    """
+    try:
+        repo = git.Repo(".", search_parent_directories=True)
+        return repo.working_tree_dir
+    except git.InvalidGitRepositoryError:
+        logger.info("Not a git repository; using current directory as root...")
+        return "."
 
 
 def return_device():
