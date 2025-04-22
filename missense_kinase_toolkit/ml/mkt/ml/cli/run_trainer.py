3529884d8ccc2976dd9ff62af963e8fbc6731c77
jessicaw9910 <jessbwhite@gmail.com>
2025-04-22 14:23:31 -0400

    run interactively line by line to debug

--- missense_kinase_toolkit/ml/mkt/ml/cli/run_trainer.py
+++ missense_kinase_toolkit/ml/mkt/ml/cli/run_trainer.py
@@ -13,16 +13,28 @@ def main():
     device = return_device()
 
     dataset_pkis2 = PKIS2Dataset()
+    # dataloader generates a batch of data that is a list of dictionaries - recreate here
+    # list_test = [i for idx, i in enumerate(dataset_pkis2.dataset_train) if idx in range(0, 5)]
 
-    train_dataloader, test_dataloader = create_dataloaders(
-        dataset_pkis2.dataset_train, dataset_pkis2.dataset_test
+    dataloader_train, dataloader_test = create_dataloaders(
+        dataset_pkis2.dataset_train, 
+        dataset_pkis2.dataset_test
     )
 
+    # for batch in dataloader_train:
+    #     print(batch)
+
+    model = CombinedPoolingModel(
+        model_name_drug=dataset_pkis2.model_drug, 
+        model_name_kinase=dataset_pkis2.model_kinase,
+    )
+
+    # model.to(device)
+
     train_model(
-        CombinedPoolingModel,
-        train_dataloader,
-        test_dataloader,
-        device,
+        model,
+        dataloader_train,
+        dataloader_test,
     )
 
     run_pipeline_with_wandb()
