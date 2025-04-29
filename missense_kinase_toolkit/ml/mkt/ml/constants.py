873095662e27c8c7ae1dd575cd72e9d994bd09cc
jessicaw9910 <jessbwhite@gmail.com>
2025-04-29 18:07:36 -0400

    have made all models Enum/StrEnum for validation purposes

--- missense_kinase_toolkit/ml/mkt/ml/constants.py
+++ missense_kinase_toolkit/ml/mkt/ml/constants.py
@@ -1,29 +1,42 @@
-from collections import defaultdict
+# from collections import defaultdict
+from enum import Enum
+from strenum import StrEnum
 
-from mkt.mk.models.pooling import CombinedPoolingModel
-from mkt.ml.datasets.pkis2 import PKIS2Dataset
+from mkt.ml.datasets.pkis2 import PKIS2KinaseSplit, PKIS2CrossValidation
+from mkt.ml.models.pooling import CombinedPoolingModel
 
-DICT_DATASET = {
-    "pkis2": PKIS2Dataset,
-}
-"""dict[str, type]: Dictionary mapping dataset names to their respective classes."""
-DICT_DATASET = defaultdict(None, DICT_DATASET)
+# DICT_DATASET = {
+#     "pkis2": PKIS2Dataset,
+# }
+# """dict[str, type]: Dictionary mapping dataset names to their respective classes."""
+# DICT_DATASET = defaultdict(None, DICT_DATASET)
 
-DICT_MODELS = {
-    "pooling": CombinedPoolingModel,
-}
-"""dict[str, type]: Dictionary mapping model names to their respective classes."""
-DICT_MODELS = defaultdict(None, DICT_MODELS)
+# DICT_MODELS = {
+#     "pooling": CombinedPoolingModel,
+# }
+# """dict[str, type]: Dictionary mapping model names to their respective classes."""
+# DICT_MODELS = defaultdict(None, DICT_MODELS)
 
-LIST_DRUG_MODELS = [
-    "DeepChem/ChemBERTa-77M-MTR",
-]
 
-LIST_KINASE_MODELS = [
-    "esm2_t6_8M_UR50D",
-    "esm2_t12_35M_UR50D",
-    "esm2_t30_150M_UR50D",
-    "esm2_t33_650M_UR50D",
-    "esm2_t36_3B_UR50D",
-    "esm2_t48_15B_UR50D",
-]
+class DataSet(Enum):
+    """Enum for dataset names."""
+    PKIS2_Kinase_Split = PKIS2KinaseSplit
+    PKIS2_CV_Split = PKIS2CrossValidation
+
+class ModelType(Enum):
+    """Enum for model types."""
+    pooling = CombinedPoolingModel
+
+class DrugModel(StrEnum):
+    """StrEnum for drug models."""
+    CHEMBERTA_MTR = "DeepChem/ChemBERTa-77M-MTR"
+    CHEMBERTA_MLM = "DeepChem/ChemBERTa-77M-MLM"
+
+class KinaseModel(StrEnum):
+    """StrEnum for kinase models."""
+    ESM2_T6_8M = "facebook/esm2_t6_8M_UR50D"
+    ESM2_T12_35M = "facebook/esm2_t12_35M_UR50D"
+    ESM2_T30_150M = "facebook/esm2_t30_150M_UR50D"
+    ESM2_T33_650M = "facebook/esm2_t33_650M_UR50D"
+    ESM2_T36_3B = "facebook/esm2_t36_3B_UR50D"
+    ESM2_T48_15B = "facebook/esm2_t48_15B_UR50D"
