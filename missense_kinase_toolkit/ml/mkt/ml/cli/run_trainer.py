617e627985cabbb5417d2aa8c834ce03fd4a936e
jessicaw9910 <jessbwhite@gmail.com>
2025-04-29 18:18:48 -0400

    added ExperimentFactory upport; TODO - pass to run_pipeline_with_wandb and trainer configs

--- missense_kinase_toolkit/ml/mkt/ml/cli/run_trainer.py
+++ missense_kinase_toolkit/ml/mkt/ml/cli/run_trainer.py
@@ -1,17 +1,41 @@
+import argparse
+
 # from mkt.ml.datasets.pkis2 import PKIS2Dataset
-from mkt.ml.log_config import configure_logging
+from mkt.ml.log_config import add_logging_flags, configure_logging
+from mkt.ml.utils import set_seed
 
 # from mkt.ml.models.pooling import CombinedPoolingModel
 # from mkt.ml.trainer import create_dataloaders, train_model
+from mkt.ml.factory import ExperimentFactory
 from mkt.ml.trainer import run_pipeline_with_wandb
 
 # from mkt.ml.utils import return_device
 
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Arguments to run the trainer.")
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to the configuration file.",
+    )
+
+    parser = add_logging_flags(parser)
+
+    return parser.parse_args()
+
 
 def main():
     """Main function to run the trainer."""
-
+    args = parse_args()
     configure_logging()
+
+    experiment = ExperimentFactory(args.config)
+    set_seed(experiment.seed)
+
+    dataset, model = experiment.build()
 
     # device = return_device()
 
