138b08ac7578c5a28ec3e785a196a4c091111a26
Jess White <50890758+jessicaw9910@users.noreply.github.com>
2025-07-22 18:52:03 -0400

    ChEMBL (#151)
    * initial implementation
    
    * added ChEMBL testing
    
    * added importlib-resources to dependencies
    
    * pinned importlib-resources version
    
    * add importlib-resources to devtools env file and removed from pyproject.toml
    
    * support for MoleculeSearch, MoleculeExact, and MoleculePreferred
    
    * add verbose flagging to ChEMBL; return_chembl_id to make queries hierarchically
    
    * docstrings for existing API clients; initial implementation of GraphQLClient
    
    * opentargets module initial implementation
    
    * test for open_targets
    
    * updated tests with Search, Exact, and Preferred for ChEMBL

--- missense_kinase_toolkit/databases/mkt/databases/api_schema.py
+++ missense_kinase_toolkit/databases/mkt/databases/api_schema.py
@@ -1,16 +1,19 @@
 import logging
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import requests
 from bravado.client import SwaggerClient
 from bravado.requests_client import RequestsClient
+from mkt.databases.requests_wrapper import get_cached_session
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
 class APIClient:
+    """Base class for API clients."""
+
     @staticmethod
     def check_response(res: requests.Response) -> None:
         """Check the response status code for errors."""
@@ -21,6 +24,8 @@ class APIClient:
 
 
 class SwaggerAPIClient(APIClient, ABC):
+    """Base class for Swagger API clients."""
+
     @abstractmethod
     def query_api(self) -> SwaggerClient:
         """Query a Swagger API and return result.
@@ -39,6 +44,8 @@ class SwaggerAPIClient(APIClient, ABC):
 
 
 class APIKeySwaggerClient(SwaggerAPIClient, ABC):
+    """Base class for Swagger API clients with API key support."""
+
     @abstractmethod
     def maybe_get_token(self) -> str | None:
         """Get API token, if available.
@@ -75,11 +82,15 @@ class APIKeySwaggerClient(SwaggerAPIClient, ABC):
 
 
 class RESTAPIClient(APIClient, ABC):
+    """Base class for REST API clients."""
+
     @abstractmethod
     def query_api(self): ...
 
 
 class APIKeyRESTAPIClient(RESTAPIClient, ABC):
+    """Base class for REST API clients with API key support."""
+
     @abstractmethod
     def maybe_get_token(self) -> str | None:
         """Get API token, if available.
@@ -103,3 +114,48 @@ class APIKeyRESTAPIClient(RESTAPIClient, ABC):
         """
         token = self.maybe_get_token()
         return {"Authorization": f"Bearer {token}"} if token else {}
+
+
+# currently only using for OpenTargets API - may want to generalize later
+@dataclass
+class GraphQLClient(APIClient):
+    """Base class for GraphQL API clients."""
+
+    url: str | None = None
+    """GraphQL API URL."""
+    query_string: str | None = None
+    """GraphQL query string to be executed."""
+    variables: dict = field(default_factory=dict)
+    """Variables for the GraphQL query, if any."""
+    response: dict | None = None
+    """Response from the GraphQL API query, if any."""
+
+    def __post_init__(self):
+        """Initialize the GraphQL API client."""
+        if self.url is None:
+            raise ValueError("API URL must be provided.")
+        if self.query_string is None:
+            raise ValueError("Query string must be provided.")
+        try:
+            self.response = self.query_api()
+        except requests.exceptions.RequestException as e:
+            logger.error("Error querying GraphQL API: %s", e)
+            raise
+
+    def query_api(self) -> dict:
+        """Query a GraphQL API and return result.
+
+        Returns
+        -------
+        dict
+            API response
+        """
+        res = get_cached_session().post(
+            self.url,
+            json={
+                "query": self.query_string,
+                "variables": self.variables,
+            },
+        )
+        res.raise_for_status()
+        return res.json()
