f156b6e1a963c2fec3c10659ac95fd604d07f1eb
jessicaw9910 <jessbwhite@gmail.com>
2025-10-06 15:26:25 -0400

    implemented new class ChEMBLMolecule to query for molecule details

--- missense_kinase_toolkit/databases/mkt/databases/chembl.py
+++ missense_kinase_toolkit/databases/mkt/databases/chembl.py
@@ -135,3 +135,77 @@ def return_chembl_id(drug: str):
         chembl_id, source = None, None
 
     return chembl_id, source
+
+
+@dataclass
+class ChEMBLMolecule(ChEMBL):
+    """ChEMBL molecule API client."""
+
+    url_suffix: str = "/molecule"
+    """URL suffix for querying exact molecule match in ChEMBL."""
+    params: dict = field(
+        default_factory=lambda: {
+            "molecule_synonyms__molecule_synonym__iexact": "<ID>", 
+            "format": "json"
+        }
+    )
+    """Parameters for the molecule API query."""
+
+    def check_molecules(self) -> str | None:
+        """Return the SMILES string for the queried molecule."""
+        if self._json is not None and "molecules" in self._json:
+            if len(self._json["molecules"]) == 0:
+                if self.verbose:
+                    logger.error(f"No molecules found in the response for {self.id}.")
+                return None
+            if len(self._json["molecules"]) > 1:
+                if self.verbose:
+                    logger.warning(f"Multiple molecules found for {self.id}. Returning the first one.")
+        else:
+            if self.verbose:
+                logger.error(f"No molecules found in the response for {self.id}.")
+            return None
+
+    def return_smiles(self) -> str | None:
+        """Return the SMILES string for the queried molecule."""
+        self.check_molecules()
+        return self._json["molecules"][0].get("molecule_structures", {}).get("canonical_smiles", None)
+
+    def return_preferred_name(self) -> str | None:
+        """Return the preferred name for the queried molecule."""
+        self.check_molecules()
+        return self._json["molecules"][0].get("pref_name", None)
+
+    def adjudicate_preferred_name(
+        self, 
+        str_in: str | None = None
+    ) -> str:
+        """Return the adjudicated preferred name for the queried molecule.
+        
+        Parameters
+        ----------
+        str_in : str | None
+            The input string to adjudicate the preferred name. If None, will not compare to input string.
+
+        Returns
+        -------
+        str
+            The adjudicated preferred name or the original ID if no preferred name is found or matches the input string.
+        """
+        preferred_name = self.return_preferred_name()
+        if preferred_name is not None:
+            # prefer INN to internal identifier
+            if str_in is not None:
+                if "-" in str_in and "-" not in preferred_name:
+                    return preferred_name.title()
+                elif "-" in preferred_name and "-" not in str_in:
+                    return str_in
+                else:
+                    return preferred_name.title()
+            return preferred_name.title()
+        else:
+            if str_in is not None:
+                return str_in.title()
+            else:
+                logger.error(f"No preferred name found for {self.id}. Returning original ID.")
+                return self.id.upper()
