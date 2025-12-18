456e130659cb12dbad8af6ed8eba12fd74504417
jessicaw9910 <jessbwhite@gmail.com>
2025-12-18 12:23:34 -0500

    created generate_sequence_and_structure_viewers function and used that in CLI rather than structure and sequence classes

--- missense_kinase_toolkit/databases/mkt/databases/cli/generate_pymol_files.py
+++ missense_kinase_toolkit/databases/mkt/databases/cli/generate_pymol_files.py
@@ -3,14 +3,11 @@
 import argparse
 from os import path
 
-from mkt.databases.app.sequences import SequenceAlignment
-from mkt.databases.app.structures import StructureVisualizer
+from mkt.databases.app.utils import generate_sequence_and_structure_viewers
 from mkt.databases.colors import DICT_COLORS
 from mkt.databases.log_config import add_logging_flags, configure_logging
 from mkt.databases.pymol import PyMOLGenerator
-from mkt.schema.io_utils import deserialize_kinase_dict, get_repo_root
-
-DICT_KINASE = deserialize_kinase_dict(str_name="DICT_KINASE")
+from mkt.schema.io_utils import get_repo_root
 
 
 def get_parser():
@@ -40,6 +37,7 @@ def get_parser():
 
 def main():
     args = get_parser().parse_args()
+
     if args.verbose == "DEBUG":
         configure_logging(True)
     else:
@@ -47,26 +45,17 @@ def main():
 
     gene = args.gene
 
-    obj_temp = DICT_KINASE[gene]
-
-    obj_alignment = SequenceAlignment(
-        obj_temp,
-        DICT_COLORS["ALPHABET_PROJECT"]["DICT_COLORS"],
-    )
-
-    viz = StructureVisualizer(
-        obj_temp,
-        obj_alignment.dict_align,
+    _, obj_viz = generate_sequence_and_structure_viewers(
+        str_kinase=gene,
+        dict_colors=DICT_COLORS["ALPHABET_PROJECT"]["DICT_COLORS"],
         str_attr="KLIFS",
     )
 
-    pymol_generator = PyMOLGenerator(viz=viz)
-
+    pymol_generator = PyMOLGenerator(viz=obj_viz)
     if args.output_dir:
         output_directory = args.output_dir
     else:
         output_directory = path.join(get_repo_root(), "images", "pymol_output", gene)
-
     pymol_generator.save_pymol_files(output_directory)
 
 
